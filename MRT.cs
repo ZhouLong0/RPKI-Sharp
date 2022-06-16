5b1dc1cd1a4d44fa9759faa7a223cd70f1349819
Zhou Long <lon.zhou@stud.uniroma3.it>
2022-06-16 18:23:32 +0200

    global stats

--- MRT.cs
+++ MRT.cs
@@ -15,14 +15,23 @@ namespace RPKIdecoder
     {
         public static void MRTWriteResult()
         {
-            TextWriter tsw = new StreamWriter(@"C:\Users\zhoul\Desktop\MRT20120601.txt");
+            /* *****CHANGE PATH*****           Table output file*/
+            TextWriter tsw = new StreamWriter(@"C:\Users\zhoul\Desktop\MRT20201225.txt");
+            /* *****CHANGE PATH*****           Stats output file*/
+            TextWriter statsFile = new StreamWriter(@"C:\Users\zhoul\Desktop\MRT20201225stats.txt");
+
             tsw.WriteLine("Prefix\tAsNumber\tExistRecord\tValid\tNCertificates\tNAuthAs\tAuthAs's");
 
+            /* *****CHANGE PATH*****           MRT table file*/
             FileInfo file = new FileInfo(@"C:\Users\zhoul\Desktop\bview.20120601.0000");
+            /* *****CHANGE PATH*****           Roa repository*/
             String directoryWithCertificates = @"C:\Users\zhoul\Desktop\repo20120601";
 
+            /*Decoded certificates*/
             List<ROA> decodedRoas = DirectoryDecoder.decodeVerifiedRoas(directoryWithCertificates);
             List<CRL> decodedCrls = DirectoryDecoder.decodeVerifiedCrls(directoryWithCertificates);
+
+            GlobalStats stats = new GlobalStats();
 
             MRTParser.Run<TableDumpRIBRecord>(file, (mrtEntry) =>
             {
@@ -30,21 +39,34 @@ namespace RPKIdecoder
 
                 foreach (TableDumpRIBEntry tdrEntry in mrtEntry.Entries)
                 {
+                    /*Do something only if it has peerAs = 3333*/
                     if (tdrEntry.Peer.PeerAS == 3333)
                     {
                         /*Do something only if it has peerAs = 3333*/
                         uint asNumber = (uint)tdrEntry.BGPUpdate.ASPath[0].ASNumbers.GetValue(tdrEntry.BGPUpdate.ASPath[0].ASNumbers.Length-1);
 
+                        /*Write an output line*/
                         RibTableLine tableLine = ROA.createTableLine(decodedRoas, decodedCrls, prefixOfEntry, asNumber);
+                        stats.lineUpdate(tableLine);
+
+                        /*Write it in the file*/
                         tsw.WriteLine(tableLine);
                      
 
                     }
                 }
 
-            });
-            tsw.Close();
 
+
+            });
+            /* Create global stats */
+            stats.globalUpdate();
+
+            statsFile.WriteLine(stats);
+
+
+            tsw.Close();
+            statsFile.Close();
 
         }
 
@@ -53,14 +75,6 @@ namespace RPKIdecoder
 
             MRTWriteResult();
 
-            //FileInfo file = new FileInfo(@"C:\Users\zhoul\Desktop\bview.20120601.0000");
-            //MRTParser.Run<TableDumpRIBRecord>(file, (mrtEntry) =>
-            //{
-
-                
-            //    Console.WriteLine(mrtEntry);
-
-            //});
 
         }
     }
